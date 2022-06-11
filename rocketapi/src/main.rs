096bae34109637e9f4cae8bd2ab88227132cea55
P3t3rtm <33187689+P3t3rtm@users.noreply.github.com>
2022-06-10 21:42:15 -0600

    testing

--- rocketapi/src/main.rs
+++ rocketapi/src/main.rs
@@ -5,7 +5,15 @@ fn index() -> &'static str {
     "Hello, world! tesing 123"
 }
 
+#[get("/world")]              // <- route attribute
+fn world() -> &'static str {  // <- request handler
+    "hello, world! gay"
+}
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build()
+        .mount("/", routes![index])
+        .mount("/testing", routes![world,index])
+        .mount("/testing123", routes![world])
 }
