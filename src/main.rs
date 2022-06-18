c95a89452dbaddc77f47685edc95e3d4bff0517b
P3t3rtm <33187689+P3t3rtm@users.noreply.github.com>
2022-06-18 01:15:16 -0600

    file server

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,59 @@
 /* #region  japanese imports */
 #[macro_use]
 extern crate rocket;
+use rocket::fs::FileServer;
 use rocket_db_pools::sqlx::{self, Row};
 use rocket_db_pools::{Connection, Database};
+use rocket::http::Status;
+use rocket::request::{self, Outcome, Request, FromRequest};
 #[derive(Database)]
 #[database("db_rocket")]
 struct Data(sqlx::MySqlPool);
 
 /* #endregion */
 
+
+
+
+
+
+struct ApiKey<'r>(&'r str);
+
+#[derive(Debug)]
+enum ApiKeyError {
+    Missing,
+    Invalid,
+}
+
+#[rocket::async_trait]
+impl<'r> FromRequest<'r> for ApiKey<'r> {
+    type Error = ApiKeyError;
+
+    async fn from_request(req: &'r Request<'_>) -> Outcome<Self, Self::Error> {
+        /// Returns true if `key` is a valid API key string.
+        fn is_valid(key: &str) -> bool {
+            key == "valid_api_key"
+        }
+
+        match req.headers().get_one("x-api-key") {
+            None => Outcome::Failure((Status::Forbidden, ApiKeyError::Missing)),
+            Some(key) if is_valid(key) => Outcome::Success(ApiKey(key)),
+            Some(_) => Outcome::Failure((Status::Forbidden, ApiKeyError::Invalid)),
+        }
+    }
+}
+
+
+
+
+
+
 /* #region  routes */
+
+#[get("/sensitive")]
+fn sensitive(key: ApiKey<'_>) -> &'static str {
+    "Sensitive data."
+}
 
 #[get("/")]
 fn index() -> &'static str {
@@ -40,8 +84,11 @@ async fn delay(seconds: u64) -> String {
 fn rocket() -> _ {
     rocket::build()
         .attach(Data::init())
+        //mounts are the front part of the urls
         .mount("/", routes![index, delay])
         .mount("/2/testing", routes![read, index])
         .mount("/test2", routes![read])
+        .mount("/sensitive", routes![sensitive])
+        .mount("/public", FileServer::from("static/"))
 }
 /* #endregion */
